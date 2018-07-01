f49f0fb5524f10156532174202aad18d8cbb0d12
Kate <ms.katesy@gmail.com>
2018-07-01 16:43:38 +1000

    Online Bank

--- Kate/wk02/4-thu/app.js
+++ Kate/wk02/4-thu/app.js
@@ -1,15 +1,15 @@
 var savBal = 0;
-var savInput = document.querySelector(".sav .user-input");
-var savBalOutput = document.querySelector(".sav .bal-output");
-var savWithdrawBtn = document.querySelector(".sav .withdraw-btn");
-var savDepBtn = document.querySelector(".sav .dep-btn");
+var savInput = document.querySelector(".sav .user-input"); //.value
+var savOutput = document.querySelector(".sav .bal-output"); //.textContent
+var savWithdrawBtn = document.querySelector(".sav .withdraw-btn"); //.addEventListener
+var savDepBtn = document.querySelector(".sav .dep-btn"); //.addEventListener
 
 
-// var checkBal = 0;
-// var checkInput = document.querySelector(".check .user-input");
-// var checkBalOutput = document.querySelector(".check .bal-output");
-// var checkWithdrawBtn = document.querySelector(".check .withdraw-btn");
-// var checkDepBtn = document.querySelector(".check .dep-btn");
+var checkBal = 0;
+var checkInput = document.querySelector(".check .user-input");
+var checkOutput = document.querySelector(".check .bal-output");
+var checkWithdrawBtn = document.querySelector(".check .withdraw-btn");
+var checkDepBtn = document.querySelector(".check .dep-btn");
 
 
 var lowBalWarning = function(){
@@ -19,64 +19,92 @@ var lowBalWarning = function(){
     else {
         document.querySelector(".sav").style.backgroundColor = "#e6e8ed";
     }
-}
-lowBalWarning();
-
-// var checkingLowBalWarning = function(){
-    // if (savBal === 0){
-        // document.querySelector(".sav").style.backgroundColor = "red";
-    // }
-    // else {
-        // document.querySelector(".sav").style.backgroundColor = "white";
-    // }
-// }
-// checkingLowBalWarning();
-
-
-
-// var depUpdate = function(acctBal, acctInput, acctBalOutput){
-    // acctBal = (acctBal + Number(acctInput.value));
-    // acctBalOutput.textContent = ("$ " + acctBal + ".00");
-    // lowBalWarning();
-// };
-// 
-// depUpdate(savBal, savInput, savBalOutput);
-
-
-var savDepUpdate = function(){
-    savBal = (savBal + Number(savInput.value));
-    savBalOutput.textContent = ("$ " + savBal + ".00");;
-    lowBalWarning()
-};
-// 
-savDepBtn.addEventListener("click", savDepUpdate);
-
-
-var savWithdrawUpdate = function() {
-    if (Number(savInput.value)>savBal){
-        alert("You don't have that much money. Go fight in a Pokemon gym to get more!");
-        return
+    if (checkBal === 0){
+        document.querySelector(".check").style.backgroundColor = "red";
     }
     else {
-        savBal = (savBal - Number(savInput.value));
-        savBalOutput.textContent = ("$ " + savBal + ".00");
-        lowBalWarning()
-    }   
-};
+        document.querySelector(".check").style.backgroundColor = "#e6e8ed";
+    }
+}
 
-savWithdrawBtn.addEventListener("click", savWithdrawUpdate);
+lowBalWarning();
 
 
+var deposit = function(acctBal, acctOutput){
+    acctOutput.textContent = ("$ " + acctBal.toFixed(2));
+    lowBalWarning();
+}
+
+var withdraw = function(acctBal,acctOutput){
+    acctOutput.textContent = ("$ " + acctBal.toFixed(2));
+    lowBalWarning();
+}
 
 
 
-// var checkDepUpdate = function(){
+savDepBtn.addEventListener("click", function(){ 
+    savBal += Number(savInput.value);
+    deposit(savBal,savOutput);
+})
+
+checkDepBtn.addEventListener("click", function(){
+    checkBal += Number(checkInput.value);
+    deposit(checkBal, checkOutput);
+})
+
+savWithdrawBtn.addEventListener("click", function(){
+    if (Number(savInput.value)>savBal){
+        alert("You are broke");
+        return
+    }
+    else{
+    savBal -=Number(savInput.value);
+    withdraw(savBal,savOutput);
+    }
+})
+
+checkWithdrawBtn.addEventListener("click", function(){
+    if (Number(checkInput.value)>checkBal){
+        alert("You are broke");
+        return
+    }
+    else{
+    checkBal -=Number(checkInput.value);
+    withdraw(checkBal,checkOutput);
+    }    
+})
+
+
+
+
+
+
+//ORIGINAL SAVINGS WITHDRAWAL FUNCTION
+// var savWithdrawUpdate = function() {
+    // if (Number(savInput.value)>savBal){
+        // alert("You don't have that much money. Go fight in a Pokemon gym to get more!");
+        // return
+    // }
+    // else {
+        // savBal = (savBal - Number(savInput.value));
+        // savOutput.textContent = ("$ " + savBal + ".00");
+        // lowBalWarning();
+    // }
+// }
+
+// savWithdrawBtn.addEventListener("click", savWithdrawUpdate);
+
+
+
+
+
+// var checkdeposit = function(){
     // checkBal = (checkBal + Number(checkInput.value));
-    // checkBalOutput.textContent = ("$ " + checkBal + ".00");;
+    // checkOutput.textContent = ("$ " + checkBal + ".00");;
     // checkingLowBalWarning()
 // };
 // 
-// checkDepBtn.addEventListener("click", checkDepUpdate);
+// checkDepBtn.addEventListener("click", checkdeposit);
 // 
 // 
 // var checkWithdrawUpdate = function() {
@@ -86,7 +114,7 @@ savWithdrawBtn.addEventListener("click", savWithdrawUpdate);
     // }
     // else {
         // checkBal = (checkBal - Number(checkInput.value));
-        // checkBalOutput.textContent = ("$ " + checkBal + ".00");
+        // checkOutput.textContent = ("$ " + checkBal + ".00");
         // checkingLowBalWarning()
     // }   
 // };
