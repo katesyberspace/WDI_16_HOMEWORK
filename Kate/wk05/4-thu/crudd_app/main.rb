a91dac9016787ddf41f43fa5776b74efb6b1c19d
Kate <ms.katesy@gmail.com>
2018-07-20 08:16:27 +1000

    added function to add new rider to the app

--- Kate/wk05/4-thu/crudd_app/main.rb
+++ Kate/wk05/4-thu/crudd_app/main.rb
@@ -12,10 +12,22 @@ get '/' do
   erb :index
 end
 
+get '/riders/new' do
+  erb :new
+end
+
 get '/riders/:rider_id' do
   @rider = Rider.find(params[:rider_id])
   erb :rider
 end
 
+post '/riders' do 
+  rider = Rider.create(
+    name: params[:name],
+    age: params[:age],
+    board_at: params[:board_at],
+    alight_at: params[:alight_at])
 
+  redirect '/'
+end
 
