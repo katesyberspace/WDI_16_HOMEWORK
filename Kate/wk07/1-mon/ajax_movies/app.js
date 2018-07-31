ee2145b50f41f3ad9946703d7d23e414293d7697
Kate <ms.katesy@gmail.com>
2018-07-31 19:59:41 +1000

    wk07 Tuesday HW, ajax movies

---
+++ Kate/wk07/1-mon/ajax_movies/app.js
@@ -0,0 +1,35 @@
+var input = document.querySelector('input')
+var button = document.querySelector('button')
+var result = document.querySelector('.result')
+
+
+button.addEventListener('click', function(event){
+  result.innerHTML = '';
+  var options = {
+    url: 'http://omdbapi.com/',
+    method: 'get',
+    dataType: 'json',
+    data: { 
+      apikey: '2f6435d9',
+      s: input.value  
+    }
+  }
+  
+  $.ajax(options).done(function(res){
+    var yourResults = document.createElement('p');
+    yourResults.classList.add('your-results');
+    yourResults.textContent = "search results:";
+    result.appendChild(yourResults);
+    res.Search.forEach(function(movie){
+      var title = movie.Title;
+      var id = movie.imdbID;
+      var p = document.createElement('p');
+      var a = document.createElement('a');
+      a.href = ("https://www.imdb.com/title/"+id);
+      a.target = "_blank"
+      a.textContent = title;
+      p.appendChild(a);
+      result.appendChild(p);
+    });
+  });
+});
