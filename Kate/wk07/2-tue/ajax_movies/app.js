cb043ec754acdad0352e0458a92ce5b69e60d693
Kate <ms.katesy@gmail.com>
2018-08-01 19:55:54 +1000

    rewrote with jquery, added movie poster and animations to poster

--- Kate/wk07/2-tue/ajax_movies/app.js
+++ Kate/wk07/2-tue/ajax_movies/app.js
@@ -1,34 +1,42 @@
-var input = document.querySelector('input')
-var button = document.querySelector('button')
-var result = document.querySelector('.result')
-
-button.addEventListener('click', function(event){
-  result.innerHTML = '';
+var handleSubmit = function(event){
+  event.preventDefault();
+  $('.result').html('');
   var options = {
     url: 'http://omdbapi.com/',
     method: 'get',
     dataType: 'json',
     data: { 
       apikey: '2f6435d9',
-      s: input.value  
+      s: $('input').val(),
+      type: 'movie'
     }
   }
   
-  $.ajax(options).done(function(res){
-    var yourResults = document.createElement('p');
-    yourResults.classList.add('your-results');
-    yourResults.textContent = "search results:";
-    result.appendChild(yourResults);
+  const showSearchResults = function(res){
     res.Search.forEach(function(movie){
       var title = movie.Title;
       var id = movie.imdbID;
+      var poster = movie.Poster;
+      var movieDiv = document.createElement('div');
+      movieDiv.className = "movie";
+      var imageDiv = document.createElement('div');
+      imageDiv.className = "img-wrapper";
+      var moviePoster = document.createElement('img');
+      moviePoster.className = "poster";
+      moviePoster.src = poster;
+      $(imageDiv).append(moviePoster);
       var p = document.createElement('p');
       var a = document.createElement('a');
       a.href = ("https://www.imdb.com/title/"+id);
       a.target = "_blank"
       a.textContent = title;
-      p.appendChild(a);
-      result.appendChild(p);
+      $(p).append(a);
+      $(movieDiv).append(imageDiv,p);
+      $('.result').append(movieDiv);
     });
-  });
-});
+  }
+
+  $.ajax(options).done(showSearchResults);
+}
+
+$('.search-form').submit(handleSubmit);
