f924ffd7dc77fd52bdeb7dcf586b5c85e9dd1e20
Kate <ms.katesy@gmail.com>
2018-08-06 16:39:24 +1000

    added 1000divs with style class and appended to body

--- Kate/wk08/1-mon/pixart/app.js
+++ Kate/wk08/1-mon/pixart/app.js
@@ -2,9 +2,11 @@ var colorForm = document.querySelector('.color-form');
 var colorInput = document.querySelector('.color-input');
 // var colorBtn = document.querySelector('button')
 var brush = $('.brush');
+var body = $('body');
 
 
 var clickHandler = function(event){
+  event.preventDefault()
   if (event.target.classList.contains('color-btn')){
     var color = colorInput.value
     $(brush).css("background-color", color)
@@ -12,5 +14,15 @@ var clickHandler = function(event){
   }
 }
 
-
 colorForm.addEventListener('click', clickHandler)
+
+var createSquares = function(){
+  for (var i = 0; i<1000; i++){
+    var square = document.createElement('div');
+    square.classList.add('square');
+    $(body).append(square);
+  }
+}
+
+
+createSquares();
