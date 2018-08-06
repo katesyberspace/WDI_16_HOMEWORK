3acf9ad474c84433f651c324bbfab3fe5aca2de2
Kate <ms.katesy@gmail.com>
2018-08-06 17:19:08 +1000

    added form to let get imdb movie poster and set as body bg

--- Kate/wk08/1-mon/pixart/app.js
+++ Kate/wk08/1-mon/pixart/app.js
@@ -3,6 +3,9 @@ var colorInput = document.querySelector('.color-input');
 // var colorBtn = document.querySelector('button')
 var brush = $('.brush');
 var $body = $('body');
+var $imdbForm = $('.imdb-form');
+var $imdbInput = $('.imdb-input');
+var $imdbBtn = $('.imdb-btn');
 
 
 var clickHandler = function(event){
@@ -33,4 +36,32 @@ var changeSquareColor = function(event){
   $target.css("background-color", color)
 }
 
-$body.on('mouseover', '.square', changeSquareColor)
+$body.on('mouseover', '.square', changeSquareColor);
+
+
+var imdbClickHandler = function(event){
+  event.preventDefault();
+  $('.result').html('');
+  var options = {
+    url: 'http://omdbapi.com/',
+    method: 'get',
+    dataType: 'json',
+    data: { 
+      apikey: '2f6435d9',
+      t: $imdbInput.val(),
+      type: 'movie'
+    }
+  }
+    
+  const setBackgroundImage = function(res){
+    $body.css('background-image', `url("${res.Poster}")`);
+    // debugger
+  };
+    
+  
+  $.ajax(options).done(setBackgroundImage);
+  
+}
+
+
+$imdbForm.on('click', '.imdb-btn', imdbClickHandler);
