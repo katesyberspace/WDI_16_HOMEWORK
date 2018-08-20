95fcc3bdb0f2731c8647abc914f0a11eefdbdcf1
Kate <ms.katesy@gmail.com>
2018-08-20 16:20:26 +1000

    added insults and named parameter

---
+++ Kate/wk10/1-mon/compliment_sandwich/controller.js
@@ -0,0 +1,7 @@
+function home(request, response){
+  response.render('home')
+}
+
+module.export = {
+  home
+}
--- Kate/wk10/1-mon/compliment_sandwich/index.js
+++ Kate/wk10/1-mon/compliment_sandwich/index.js
@@ -4,6 +4,27 @@ const PORT = 4567
 const ejs = require('ejs')
 const _ = require('lodash')
 const logger = require('./logger.js')
+// const controller = require('./controller.js')
+
+compliments = [
+"Your instructors love you",
+"High five = ^5",
+"Shut up and take my money",
+"It's almost beer o'clock",
+"The Force is strong with you"
+]
+
+insults = [
+  "you smell awful",
+  "your s looks like a 5",
+  "your pant legs are tucked into your socks",
+  "your left earlobe is longer than your right earlobe",
+  "your dog pooped on my lawn"
+  ]
+
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 
 
 //all aboard the routing train!
@@ -14,8 +35,30 @@ app.use(logger)
 app.use(express.static('public'))
 
 
-app.get('/', function(request, response){
-
+app.get('/', function(req, res){
+  let compliment = _.sample(compliments)
+  let color = _.sample(colors)
+  res.render('home', {
+    compliment: compliment,
+    color: color
+  })
 })
 
 
+app.get('/:name', function(req, res){
+  let params = req.params
+  let compliment1 = _.sample(compliments)
+  let compliment2 = _.sample(compliments)
+  let insult = _.sample(insults)
+  let color = _.sample(colors)
+  params['compliment1'] = compliment1
+  params['compliment2'] = compliment2
+  params['insult'] = insult
+  params['color'] = color
+  res.render('name', params)
+})
+
+
+app.listen(PORT, () => {
+  console.log(`listening at ${PORT}`)
+})
